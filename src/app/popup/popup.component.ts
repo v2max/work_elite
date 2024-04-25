9e66dd65bc9f07433501cbebccaf1837f3d62011
v2max <164172366+v2max@users.noreply.github.com>
2024-04-26 00:20:30 +0530

    wish list duplicating is checked

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,46 +1,94 @@
+import { NgFor } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.scss']
+  styleUrls: ['./popup.component.scss'],
 })
 export class PopupComponent {
-
   [x: string]: any;
   boxset: any;
-  // wishlist:any = [];
 
   constructor(
     public dialogRef: MatDialogRef<PopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA)
+    public data: any
   ) {
     console.log('djfhjdhfjdh', data);
+  }
+
+     wish() {
+    let existWish = localStorage.getItem('wishlist');
+    let wishlist = existWish ? JSON.parse(existWish) : [];
+
+    let list = this.data;
+    let isAlreadyInWishlist = false;
+    
+
+    console.log(list, 'data for list');
+
+    if (wishlist.length == 0) {
+
+      wishlist.push(list);
+      let stringfiy = JSON.stringify(wishlist);
+      localStorage.setItem('wishlist', stringfiy);
+      console.log('length');
+
+    } else {
+      console.log('in for');
+
+      for (let i = 0; i < wishlist.length; i++) {
+
+        if (isEqual(wishlist[i],list)) {
+
+          console.log('Item already exists in wishlist');
+          isAlreadyInWishlist = true;
+          break;
+        }
+      }
+
+      if (!isAlreadyInWishlist) {
+        wishlist.push(list);
+        let stringify = JSON.stringify(wishlist);
+        localStorage.setItem('wishlist', stringify);
+        console.log('Item added to wishlist');
+      
+      } else {
+        
+         false;
+      }
+    }
+      
+      
+          
+        
+      
+    
   }
 
   close(): void {
     this.dialogRef.close();
   }
+}
 
-  wish(){
+function isEqual(obj1: { [x: string]: any; }, obj2: { [x: string]: any; }) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
-    let existWish = localStorage.getItem('wishlist')
-    let wishlist:any = existWish ? JSON.parse(existWish) : [];
-    
-    let list = this.data;
-
-    console.log(list,"data for list");
-
-     wishlist.push(list);
-
-    console.log(wishlist,"fitted in arry");
-
-    let stringfiy = JSON.stringify(wishlist);
-
-    localStorage.setItem("wishlist",stringfiy);
-
-
+  if (keys1.length !== keys2.length) {
+    return false;
   }
 
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+
+
