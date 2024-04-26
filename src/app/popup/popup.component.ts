3d50e8a0ff432e5daa99c929eb9d1c1fa958afaa
v2max <164172366+v2max@users.noreply.github.com>
2024-04-26 17:45:06 +0530

    wish already exists

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,5 +1,4 @@
-import { NgFor } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -7,43 +6,42 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.scss'],
 })
-export class PopupComponent {
+export class PopupComponent implements OnInit {
   [x: string]: any;
   boxset: any;
 
   constructor(
     public dialogRef: MatDialogRef<PopupComponent>,
-    @Inject(MAT_DIALOG_DATA)
-    public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     console.log('djfhjdhfjdh', data);
   }
 
-     wish() {
+  ngOnInit() {
+    this.check()
+  }
+
+  wish() {
     let existWish = localStorage.getItem('wishlist');
     let wishlist = existWish ? JSON.parse(existWish) : [];
 
     let list = this.data;
     let isAlreadyInWishlist = false;
-    
 
     console.log(list, 'data for list');
 
     if (wishlist.length == 0) {
-
-      wishlist.push(list);
-      let stringfiy = JSON.stringify(wishlist);
+       wishlist.push( list);
+      let stringfiy = JSON.stringify( wishlist);
       localStorage.setItem('wishlist', stringfiy);
       console.log('length');
-
     } else {
       console.log('in for');
 
       for (let i = 0; i < wishlist.length; i++) {
-
-        if (isEqual(wishlist[i],list)) {
-
+        if (isEqual(wishlist[i], list)) {
           console.log('Item already exists in wishlist');
+          setbtn();
           isAlreadyInWishlist = true;
           break;
         }
@@ -54,18 +52,38 @@ export class PopupComponent {
         let stringify = JSON.stringify(wishlist);
         localStorage.setItem('wishlist', stringify);
         console.log('Item added to wishlist');
-      
       } else {
-        
-         false;
+        false;
       }
     }
-      
-      
-          
+  }
+
+  check(){
+
+    let existWish = localStorage.getItem('wishlist');
+    let wishlist = existWish ? JSON.parse(existWish) : [];
+
+    let list = this.data;
+
+    if (wishlist.length == 0) {
+      wishlist.push( list);
+     let stringfiy = JSON.stringify( wishlist);
+     localStorage.setItem('wishlist', stringfiy);
+   }else{
+
+    for (let i = 0; i < wishlist.length; i++) {
+      if (isEqual(wishlist[i], list)) {
         
-      
-    
+        setbtn();
+        break;
+
+      }else{
+        false;
+      }
+    }
+
+   }
+
   }
 
   close(): void {
@@ -73,8 +91,13 @@ export class PopupComponent {
   }
 }
 
+function setbtn() {
+  const myButton = document.querySelector('#wishbtn') as HTMLButtonElement;
+  myButton.textContent = 'Exists in Wishlist';
+  myButton.disabled = true;
+}
 
-function isEqual(obj1: { [x: string]: any; }, obj2: { [x: string]: any; }) {
+function isEqual(obj1: { [x: string]: any }, obj2: { [x: string]: any }) {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
 
@@ -90,6 +113,3 @@ function isEqual(obj1: { [x: string]: any; }, obj2: { [x: string]: any; }) {
 
   return true;
 }
-
-
-
